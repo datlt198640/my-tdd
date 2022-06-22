1b7c041c232161d6a536737bedbda8467f929575
dat le <dat.lt198640@gmail.com>
2022-06-22 16:16:50 +0700

    add tag api

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -10,6 +10,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """ Test model."""
 
@@ -74,3 +79,10 @@ class ModelTest(TestCase):
             description='Sample receipe description.',
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
